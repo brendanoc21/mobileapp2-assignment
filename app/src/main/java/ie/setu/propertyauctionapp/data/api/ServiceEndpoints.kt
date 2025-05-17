be59394a77e1e0b6069b14a8b2c81580180b3b51
Brendan O'Connor <brendanoconnor2030@gmail.com>
2025-05-17 11:26:02 +0100

    Remove Room files and fix errors

--- app/src/main/java/ie/setu/propertyauctionapp/data/api/ServiceEndpoints.kt
+++
@@ -1,8 +0,0 @@
-package ie.setu.propertyauctionapp.data.api
-
-class ServiceEndPoints {
-    companion object {
-        const val BASE_URL= "https://donationxweb-multi-server.onrender.com/"
-        const val AUCTIONS_ENDPOINT = "auctions"
-    }
-}
